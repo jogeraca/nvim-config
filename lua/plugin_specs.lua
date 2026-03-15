e0f16ff3e24570d780a5723f2dcd33279a84bc2d
Yoser <jogeraca@gmail.com>
2026-03-15 14:11:30 -0500

    feat(ia): implement openai and claude

--- lua/plugin_specs.lua
+++ lua/plugin_specs.lua
@@ -816,6 +816,72 @@ local plugin_specs = {
   {
     "tpope/vim-projectionist", version = "*",
   },
+  {
+    "yetone/avante.nvim",
+    config = function()
+      require("config.avante")
+    end,
+    event = "VeryLazy",
+    version = "*",
+    build = "make",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter",
+      "stevearc/dressing.nvim",
+      "nvim-lua/plenary.nvim",
+      "MunifTanjim/nui.nvim",
+      --- The below dependencies are optional,
+      "echasnovski/mini.pick", -- for file_selector provider mini.pick
+      "nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+      "hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+      "ibhagwan/fzf-lua", -- for file_selector provider fzf
+      "nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+      {
+        -- support for image pasting
+        "HakonHarnes/img-clip.nvim",
+        event = "VeryLazy",
+        opts = {
+          -- recommended settings
+          default = {
+            embed_image_as_base64 = false,
+            prompt_for_file_name = false,
+            drag_and_drop = {
+              insert_mode = true,
+            },
+            -- required for Windows users
+            use_absolute_path = true,
+          },
+        },
+      },
+      {
+        -- Make sure to set this up properly if you have lazy=true
+        'MeanderingProgrammer/render-markdown.nvim',
+        opts = {
+          file_types = { "markdown", "Avante" },
+        },
+        ft = { "markdown", "Avante" },
+      },
+    },
+  },
+-- Claude Code integration
+  {
+    "coder/claudecode.nvim",
+    cmd = { "ClaudeCode", "ClaudeChat" },
+    config = function()
+      require("config.claudecode")
+    end,
+  },
+  -- CodeCompanion - AI chat with multiple adapters
+  {
+    "olimorris/codecompanion.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+    },
+    config = function()
+      require("config.codecompanion")
+    end,
+    event = "VeryLazy",
+  },
 }
 
 require("lazy").setup {
